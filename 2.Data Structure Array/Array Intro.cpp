a3380ba4f8c30bc8ecdb1e4ae5e2b930dd45cbc5
Shashi <shashisrivastav96@gmail.com>
2023-01-06 17:35:46 +0530

    arr

---
+++ 2.Data Structure Array/Array Intro.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//int Arr[10];
+// Arr[0] = 123;
+// cout<<Arr[0];
+
+//int Arr[10];
+// for (int i = 0; i < 10; i++){
+//     Arr[i] = i;
+//     cout<<Arr[i]<<" ";
+// }
+
+int Arr[10] {1,2,3};
+cout<<sizeof(Arr);
+    return 0;
+}
